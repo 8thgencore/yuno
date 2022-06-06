d453538a2514e0cf19260279dd58602c8a29eac9
8thgencore <axelmaker@ya.ru>
2022-06-07 00:14:22 +0300

    Edit Splash Screen

--- lib/app/splash/splash_page.dart
+++ lib/app/splash/splash_page.dart
@@ -34,44 +34,47 @@ class _SplashPageState extends State<SplashPage> {
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
-    return Container(
-      color: backgroundColor,
-      child: Stack(
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Stack(
         alignment: Alignment.center,
         children: [
+          SizedBox(
+            width: _deviceWidth,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const SizedBox(height: 94),
+                Image.asset(logo, width: 70, height: 70),
+                const Text(
+                  'yuno.',
+                  style: TextStyle(
+                    color: primaryTextColor,
+                    fontSize: 26,
+                    fontWeight: FontWeight.w700,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                SizedBox(height: _deviceHeight * 0.4),
+                const CircularProgressIndicator(color: primaryColor),
+                const SizedBox(height: 20),
+                const Text(
+                  'The Coolest Task Manager App',
+                  style: TextStyle(
+                    color: primaryTextColor,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                SizedBox(height: _deviceHeight * 0.2),
+              ],
+            ),
+          ),
           Positioned(
             bottom: 0,
             left: 0,
             child: Image.asset(bottomOrnament),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              const SizedBox(height: 94),
-              Image.asset(logo, width: 70, height: 70),
-              const Text(
-                'yuno.',
-                style: TextStyle(
-                  color: primaryTextColor,
-                  fontSize: 26,
-                  fontWeight: FontWeight.w700,
-                  decoration: TextDecoration.none,
-                ),
-              ),
-              SizedBox(height: _deviceHeight * 0.4),
-              const CircularProgressIndicator(color: primaryColor),
-              const SizedBox(height: 20),
-              const Text(
-                'The Coolest Task Manager App',
-                style: TextStyle(
-                  color: primaryTextColor,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  decoration: TextDecoration.none,
-                ),
-              ),
-              SizedBox(height: _deviceHeight * 0.2),
-            ],
           ),
         ],
       ),
@@ -80,7 +83,7 @@ class _SplashPageState extends State<SplashPage> {
 
   Future<void> loadData({required BuildContext context}) async {
     timer = Timer(
-      const Duration(seconds: 1000),
+      const Duration(seconds: 2),
       () => Navigator.pushNamedAndRemoveUntil(
           context, '/counter', (route) => false),
     );
